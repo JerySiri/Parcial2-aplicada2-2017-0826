f0f3991a5fb343123dc74304c3ebd368c3bf4065
Jery Javier Siri Mena <jery_sirei@ucne.edu.do>
2021-06-30 18:19:56 -0400

    Creacion de la entidad CobrosDetalles

---
+++ Models/CobrosDetalle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2_aplicada2_2017_0826.Models
+{
+    public class CobrosDetalle
+    {
+        [Key]
+        public int id { get; set; }
+
+        public int ventaId { get; set; }
+
+        public double cobrado { get; set; }
+
+        public bool fuePagado { get; set; }
+    }
+}
